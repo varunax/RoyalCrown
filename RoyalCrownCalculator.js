4d2d11d29c8e19adcc2e91c928afdc495abc49a4
Varuna <varunax@live.com>
2020-05-10 02:44:15 +0100

    fixed stats displaying incorrectly

--- RoyalCrownCalculator.js
+++ RoyalCrownCalculator.js
@@ -1,24 +1,21 @@
-var champion = [{
+var champion = {
 	patk:0,
 	matk:0,
 	pdef:0,
 	mdef:0,
-	
 	hp:0,
 	mp:0,
 	atkspd:0,
 	critrate:0,
-	
 	cdred:0,
 	hpregen:0,
 	movspd:0,
 	mpregen:0,
-	
 	ppen:0,
 	ppenPercent:0,
 	mpen:0,
 	mpenPercent:0
-}];
+};
 
 function initStats(){
 	var stats = [
@@ -44,10 +41,7 @@ function calculate(){
 	var championName = document.getElementById("champion").value;
 	var level = Number(document.getElementById("level").value);
 	
-	if(level == 0){
-		initStats();
-		return;
-	}
+	if(championName == "none" || level == 0){ initStats();return;}
 	
 	var patkBase = 0;
 	var patkGrowth = 0;
@@ -73,21 +67,77 @@ function calculate(){
 		initStats();
 	}
 	
-	champion.patk = patkBase + patkGrowth*level-patkGrowth;
-	champion.matk = matkBase;
-	champion.pdef = pdefBase + pdefGrowth*level-pdefGrowth;
-	champion.mdef = mdefBase + mdefGrowth*level-mdefGrowth;
+	var weaponName = document.getElementById("weapon").value;
+	var weaponRarity = document.getElementById("weaponRarity").value;
+	var weaponPatk = 0;
+	var weaponMatk = 0;
+	var weaponPdef = 0;
+	var weaponMdef = 0;
+	var weaponAtkspd = 0;
+	var weaponMovspd = 0;
+	
+	switch(weaponName){
+		case "steelSword":
+			switch(weaponRarity){
+				case "uncommon":
+				weaponPatk = 20;
+				weaponPdef = 18;
+				weaponMdef = 18;
+				break;
+				
+				case "rare":
+				weaponPatk = 30;
+				weaponPdef = 24;
+				weaponMdef = 24;
+				break;
+				
+				case "epic":
+				weaponPatk = 40;
+				weaponPdef = 30;
+				weaponMdef = 30;
+				break;
+			}
+		break;
+		
+		case "crescentBlade":
+			switch(weaponRarity){
+				case "uncommon":
+				weaponPatk = 25;
+				weaponAtkspd = 10;
+				weaponMovspd = 3;
+				break;
+				
+				case "rare":
+				weaponPatk = 37.5;
+				weaponAtkspd = 15;
+				weaponMovspd = 4;
+				break;
+				
+				case "epic":
+				weaponPatk = 50;
+				weaponAtkspd = 20;
+				weaponMovspd = 5;
+				break;
+			}
+		break;
+		
+	}
+	
+	champion.patk = patkBase + patkGrowth*level-patkGrowth + weaponPatk;
+	champion.matk = matkBase + weaponMatk;
+	champion.pdef = pdefBase + pdefGrowth*level-pdefGrowth + weaponPdef;
+	champion.mdef = mdefBase + mdefGrowth*level-mdefGrowth + weaponMdef;
 	champion.hp = hpBase + hpGrowth*level-hpGrowth;
 	champion.mp = mpBase + mpGrowth*level-mpGrowth ;
-	champion.atkspd = atkspdBase + atkspdGrowth*level-atkspdGrowth;
+	champion.atkspd = atkspdBase + atkspdGrowth*level-atkspdGrowth + weaponAtkspd;
+	champion.movspd = weaponMovspd;
 	
 	var i = 0;
 	var j = 0;
 	var championStats = Object.values(champion);
-	championStats.shift();
 	for(j in championStats){
 		document.getElementById(stats[i++]).innerHTML = championStats[j];
-  }
+		}
 	
 	function setStats(val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13, val14){
 		patkBase = val1;
